9393114c21f0d2b951d50cba94515b73504325f8
CaptainMumo <mumopeter17@gmail.com>
2018-11-24 21:15:11 +0300

    Created an implementation of the function using loops instead of the original list comprehension to ensure that I understand what is going on in the list comprehension

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,4 +1,17 @@
 def primeNumbers(number):
     return [x for x in range(2,number + 1) if all(x%y != 0 for y in range(2,x))]
 
+    """prime_numbers = [2]
+
+    if number <= 1:
+        return []
+    else:   
+        for x in range(2, number+1):
+            for y in range(2,x):
+                if x%y == 0: 
+                    break
+                else:   
+                    prime_numbers.append(x)
+                    break           
+        return prime_numbers """           
 
